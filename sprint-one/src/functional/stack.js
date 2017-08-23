b6f123ff724f1e398c82874135f0881660ba0c12
Lynne Daniels <lynne33477@gmail.com>
2017-08-23 11:46:49 -0700

    pass all stack tests, clean up

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,9 +1,7 @@
 var Stack = function() {
   var someInstance = {};
   var count = 0;
-
   // Use an object with numeric keys to store values
-  var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
@@ -12,7 +10,7 @@ var Stack = function() {
   };
 
   someInstance.pop = function() {
-    console.log(this.size);
+
     if (count > 0) {
       count --;
       return someInstance[count + 1]; 
