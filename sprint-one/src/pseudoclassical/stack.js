9e88094b5f10bfec2a49b7371ebd6b3495335ce3
Lynne Daniels <lynne33477@gmail.com>
2017-08-27 22:11:43 -0700

    battery

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.top = 0;
+  this.storage = {};
 };
 
+var stack = new Stack;
+Stack.prototype.size = function() {
+  return this.top;
+};
+Stack.prototype.push = function(val) {
+  this.storage[this.top] = val;
+  this.top ++;
+};
 
